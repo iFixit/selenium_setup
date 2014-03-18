56ec5a49f4ea77e2974f02a33a588f172757e079
David Rans <david@Davids-MacBook-Pro.local>
2014-03-17 17:13:44 -0700

    Add chromedriver to get_selenium script

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 curl --remote-name http://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar
+curl --remote-name http://chromedriver.storage.googleapis.com/2.9/chromedriver_mac32.zip
+unzip chromedriver_mac32.zip
+rm chromedriver_mac32.zip
