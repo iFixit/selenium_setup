6588a586af8fb87c7c8db2e389f18cd734eb0dc4
BaseInfinity <stefanayala3266@gmail.com>
2014-06-06 18:14:35 -0700

    upgrade server

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --remote-name http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar
+curl --remote-name http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar
 curl --remote-name http://chromedriver.storage.googleapis.com/2.9/chromedriver_mac32.zip
 unzip chromedriver_mac32.zip
 rm chromedriver_mac32.zip
