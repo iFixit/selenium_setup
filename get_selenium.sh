0492b11789f423b604715cbf2b1bc48ecd36de56
BaseInfinity <stefanayala3266@gmail.com>
2014-10-08 11:26:51 -0700

    Use phantomjs

--- get_selenium.sh
+++ get_selenium.sh
@@ -2,5 +2,10 @@
 
 curl --remote-name http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar
 curl --remote-name http://chromedriver.storage.googleapis.com/2.9/chromedriver_mac32.zip
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-macosx.zip
+
+unzip phantomjs*.zip
+rm phantomjs*.zip
+
 unzip chromedriver_mac32.zip
 rm chromedriver_mac32.zip
