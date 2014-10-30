dd846133d86b78429cfa8b2490b02ffc73bb7530
BaseInfinity <stefanayala3266@gmail.com>
2014-10-30 09:33:20 -0700

    Update get_selenium script

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl --remote-name http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar
-curl --remote-name http://chromedriver.storage.googleapis.com/2.9/chromedriver_mac32.zip
+curl --remote-name http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar
+curl --remote-name http://chromedriver.storage.googleapis.com/2.12/chromedriver_mac32.zip
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-macosx.zip
 
 unzip phantomjs*.zip
