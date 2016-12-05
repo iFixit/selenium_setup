9459d93bf9328208c8d5d1aa1bef45d67d15b5f8
David Rans <dave.g.rans@gmail.com>
2016-12-05 15:46:45 -0800

    Update Chromedriver, remove phantomjs
    This grabs the newest version of Chromedriver.
    
    The `wget phantomjs` never worked.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 curl --remote-name http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar
-curl --remote-name http://chromedriver.storage.googleapis.com/2.12/chromedriver_mac32.zip
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-macosx.zip
+curl --remote-name https://chromedriver.storage.googleapis.com/2.25/chromedriver_mac64.zip
 
-unzip phantomjs*.zip
-cp phantomjs-1.9.7-macosx/bin/phantomjs .
-
-rm phantomjs*.zip
-rm -r phantomjs*-macosx
-
-unzip chromedriver_mac32.zip
-rm chromedriver_mac32.zip
+unzip chromedriver_mac64.zip
+rm chromedriver_mac64.zip
