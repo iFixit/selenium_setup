c2c32b220983fbe23a11401f723f7a973f40aa45
David Rans <dave.g.rans@gmail.com>
2017-04-12 10:51:26 -0700

    Use newer chromedriver for newer version of Chrome
    I ran into the error detailed here: https://github.com/SeleniumHQ/selenium/issues/3508
    
    Upgrading to a newer version of chromedriver fixed it.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl --remote-name http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar
-curl --remote-name https://chromedriver.storage.googleapis.com/2.25/chromedriver_mac64.zip
+curl --remote-name https://chromedriver.storage.googleapis.com/2.28/chromedriver_mac64.zip
 
 unzip chromedriver_mac64.zip
 rm chromedriver_mac64.zip
