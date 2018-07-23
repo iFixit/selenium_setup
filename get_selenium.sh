203fa0f73cc411978b8e35af3c14279161a615d5
Daniel Beardsley <daniel@ifixit.com>
2018-07-23 10:08:24 -0700

    Chromedriver: update to 2.38
    Chrome changed and the old version of chromedriver is not compatible.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl --remote-name http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
-curl --remote-name https://chromedriver.storage.googleapis.com/2.33/chromedriver_mac64.zip
+curl --remote-name https://chromedriver.storage.googleapis.com/2.38/chromedriver_mac64.zip
 
 unzip chromedriver_mac64.zip
 rm chromedriver_mac64.zip
