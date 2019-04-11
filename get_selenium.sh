d27a00cf66f40d584208d9c1e0071a7e4da876a6
Andrew Gilbert <agilbert@ifixit.com>
2019-04-11 14:46:14 -0700

    Update webdriver and selenium server versions
    Updating selenium server allows it to be compatible with Java 12.
    
    The current version of chrome-webdriver is incompatible with our tests, so we
    are using the last version I believe to be compatible.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-curl --remote-name https://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
-curl --remote-name https://chromedriver.storage.googleapis.com/2.38/chromedriver_mac64.zip
+chrome_version=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version 2>&1 | sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/')
 
-unzip chromedriver_mac64.zip
-rm chromedriver_mac64.zip
+file_name=chromedriver_mac64.zip
+curl --remote-name https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
+chrome_driver_version=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_version")
+chromedriver_url="https://chromedriver.storage.googleapis.com/$chrome_driver_version/$file_name"
+
+# Override URL until Facebook's WebDriver gets fixed
+chromedriver_url="https://chromedriver.storage.googleapis.com/2.45/chromedriver_mac64.zip"
+echo "$chromedriver_url"
+curl -o "$file_name" "$chromedriver_url"
+
+unzip $(basename $file_name .zip)
+rm -f $file_name
