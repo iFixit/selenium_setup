64b8d15f5109f8ae8f40cf9cc94e7e47814b2472
Addison Grant <addison-grant@users.noreply.github.com>
2019-09-06 15:22:28 -0700

    Update get_selenium.sh

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --remote-name https://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar
+curl --remote-name https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar
 curl --remote-name https://chromedriver.storage.googleapis.com/2.38/chromedriver_mac64.zip
 
 unzip chromedriver_mac64.zip
