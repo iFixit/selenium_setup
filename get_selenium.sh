d3690d068f64194303956dbea831b9704d9e5e11
Addison Grant <addison@ifixit.com>
2019-09-06 15:36:47 -0700

    Upgrade get_selenium some more
    Use latest stable Chrome release
    Use later standalone selenium server (3.13.0 was failing for me locally).

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl --remote-name https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar
-curl --remote-name https://chromedriver.storage.googleapis.com/2.38/chromedriver_mac64.zip
+curl --remote-name https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
+curl --remote-name https://chromedriver.storage.googleapis.com/76.0.3809.126/chromedriver_mac64.zip
 
 unzip chromedriver_mac64.zip
 rm chromedriver_mac64.zip
