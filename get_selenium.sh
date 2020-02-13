d2640d8a9226d09699d1ff6046b2445506334404
Andrew Gilbert <andrewg@ifixit.com>
2020-02-13 09:48:02 -0800

    Extract duplicated code
    This unifies the version number extraction code for both platforms.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+extract_version() {
+   sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/'
+}
+
 if [ $(uname -s) == "Linux" ]
 then
-   chrome_version=$(google-chrome --version 2>&1 | sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/')
+   chrome_path=google-chrome
    file_name=chromedriver_linux64.zip
 else
-   chrome_version=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version 2>&1 | sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/')
+   chrome_path=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
    file_name=chromedriver_mac64.zip
 fi
 
+chrome_version=$("$chrome_path" --version 2>&1 | extract_version)
 curl --remote-name https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
 chrome_driver_version=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_version")
 chromedriver_url="https://chromedriver.storage.googleapis.com/$chrome_driver_version/$file_name"
