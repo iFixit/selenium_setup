026a53aad947834a349b75c2dab6a0ac30e3a288
Andrew Gilbert <andrewg@ifixit.com>
2020-02-13 09:16:48 -0800

    Add support for auto-switching between Mac and Linux
    This allows us to maintain one `get_selenium.sh` script that works on
    both platforms.

--- get_selenium.sh
+++ get_selenium.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ $(uname -s) == "Linux" ]
+then
+chrome_version=$(google-chrome --version 2>&1 | sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/')
+
+file_name=chromedriver_linux64.zip
+else
 chrome_version=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version 2>&1 | sed -E 's/^.*[[:space:]]+(([[:digit:]]|\.)+)\.[[:digit:]]+[[:space:]]+$/\1/')
 
 file_name=chromedriver_mac64.zip
+fi
 curl --remote-name https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
 chrome_driver_version=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_version")
 chromedriver_url="https://chromedriver.storage.googleapis.com/$chrome_driver_version/$file_name"
